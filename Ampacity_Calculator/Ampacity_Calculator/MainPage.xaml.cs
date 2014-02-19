3a2d1a355826d6981dcb921aceeecbbc71b7fa06
hummel86 <hummel86@students.rowan.edu>
2014-02-19 15:58:06 -0500

    Allows double inputs and power loss calculation.

--- Ampacity_Calculator/Ampacity_Calculator/MainPage.xaml.cs
+++ Ampacity_Calculator/Ampacity_Calculator/MainPage.xaml.cs
@@ -71,9 +71,9 @@ namespace Ampacity_Calculator
             int phase_index = phases.SelectedIndex;
 
             // Variable if number is valid input
-            int number;
+            double number;
 
-            //
+            // Condition turns true if a invalid input is entered
             bool error_flag = false;
 
             // Brush colors for borders
@@ -83,11 +83,9 @@ namespace Ampacity_Calculator
             // If input is valid, assign no border, else assign red border, loops through the user inputs
             for (int i = 0; i <= 3; i++)
             {
-                if (Int32.TryParse(user_inputs[i].Text, out number))
+                if (Double.TryParse(user_inputs[i].Text, out number))
                 {
                     user_inputs[i].BorderBrush = noColor;
-                    
-
                 }
                 else
                 {
@@ -97,13 +95,14 @@ namespace Ampacity_Calculator
                         error_flag = true;
                     }
                 }
-
             }
+            // If code finds errors with your inputs, it will output an error message
             if (error_flag == true)
             {
                 ResultsVD.Text = "";
                 ResultsVDper.Text = "";
                 chosenSize.Text = "";
+                powerloss.Text = "";
                 error_message.Text = "*There are errors with your inputs.";
                 return;
             }
@@ -132,38 +131,65 @@ namespace Ampacity_Calculator
             double[] Cu_res_key = {3.1,2.0,1.2,0.78,0.49,0.31,0.25,0.19,0.15,0.12,0.10,0.077,0.062,0.052,0.044,0.038,0.033,0.027,0.023,0.019};
             double[] Al_res_key = {0,3,2,2.0,1.3,0.81,0.51,0.40,0.32,0.25,0.20,0.16,0.13,0.10,0.085,0.071,0.061,0.054,0.043,0.036,0.029};
 
+            // Condition for when the correct wire size is found matching desired voltage drop
             bool correct_wire_size=false;
+
+            // Doubles to store the voltage drop and percent voltage drop
             double VD=0;
             double percent_VD=0;
+            double power=0;
+
+            // Arrays to set desired ampacity/res key to either Al or Cu
+            double[] ampacity_key;
+            double[] res_key;
+
+            // If index is 0, Cu is choosen, else Al is choosen
+            if (material_index == 0)
+            {
+                ampacity_key = Cu_ampacity_key;
+                res_key = Cu_res_key;
+            }
+            else
+            {
+                ampacity_key = Al_ampacity_key;
+                res_key = Al_res_key;
+            }
+
+            // Loop variable
             int j=0;
 
             for (j=0; correct_wire_size == false && j<=19; j++)
             {
-                if (Cu_ampacity_key[j] > I)
+                if (ampacity_key[j] > I)
                 {
                     // Calculate the voltage drop and percent VD of the system 
-                    VD = (2 * length * Cu_res_key[j] * I) / 1000;
+                    VD = (2 * length * res_key[j] * I) / 1000;
                     percent_VD = (VD / system_voltage) * 100;
 
                     if (percent_VD <= desired_VD)
                     {
                         correct_wire_size = true;
+                        power=I*I*(res_key[j]/1000)*2*length;
+                        break;
                     }
                 }  
             }
             if (correct_wire_size == true)
             {
                 // Return the results to the text boxes for the user to see
-                ResultsVD.Text = "Voltage Drop= " + VD + " V";
-                ResultsVDper.Text = "Percent VD= " + percent_VD + "%";
+                ResultsVD.Text = "Voltage Drop= " + Math.Round( VD,3) + " V";
+                ResultsVDper.Text = "Percent VD= " + Math.Round(percent_VD,3) + "%";
                 chosenSize.Text = "Wire Size: " + wire_size_key[j];
+                powerloss.Text = "Power Loss: " + power/1000 + " kW";
             }
 
             else{
+                // Output error message, clear the values for the results 
                 error_message.Text = "*Your voltage drop could not be met or too much current exists.";
                 ResultsVD.Text = "";
                 ResultsVDper.Text = "";
                 chosenSize.Text = "";
+                powerloss.Text = "";
             }
         }
     }
